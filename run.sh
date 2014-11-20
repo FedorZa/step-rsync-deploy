53821a49594b9a4af9744ec502fc35b5823cbc9a
luckymancvp <luckymancvp@gmail.com>
2014-11-20 16:37:27 +0700

    add

--- run.sh
+++ run.sh
@@ -49,7 +49,7 @@ info "Curr Directory"
 info $("pwd")
 info $("ls")
 sync_command="rsync -urltv --rsh=\"$rsync_command\" \"$source_dir\" \"$remote_user@$WERCKER_RSYNC_DEPLOY_HOST:$WERCKER_RSYNC_DEPLOY_DIRECTORY\""
-sync_output=$($sync_command)
+sync_output=$(rsync -urltv --rsh="$rsync_command" "$source_dir" "$remote_user@$WERCKER_RSYNC_DEPLOY_HOST:$WERCKER_RSYNC_DEPLOY_DIRECTORY")
 if [[ $? -ne 0 ]];then
     warning $sync_output
     fail 'rsync failed';
