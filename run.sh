f1994d405055da2984b1172c7a4840df120347e0
luckymancvp <luckymancvp@gmail.com>
2014-11-20 09:25:40 +0700

    add echo

--- run.sh
+++ run.sh
@@ -44,10 +44,10 @@ then
 fi
 
 info "Synchronizing $source_dir to $remote_user@$WERCKER_RSYNC_DEPLOY_HOST:$WERCKER_RSYNC_DEPLOY_DIRECTORY..."
+info "rsync -urltv --delete --rsh=$rsync_command $source_dir $remote_user@$WERCKER_RSYNC_DEPLOY_HOST:$WERCKER_RSYNC_DEPLOY_DIRECTORY"
 sync_output=$(rsync -urltv --delete --rsh="$rsync_command" "$source_dir" "$remote_user@$WERCKER_RSYNC_DEPLOY_HOST:$WERCKER_RSYNC_DEPLOY_DIRECTORY")
 if [[ $? -ne 0 ]];then
     warning $sync_output
-    echo rsync -urltv --delete --rsh="$rsync_command" "$source_dir" "$remote_user@$WERCKER_RSYNC_DEPLOY_HOST:$WERCKER_RSYNC_DEPLOY_DIRECTORY"
     fail 'rsync failed';
 else
     success "finished rsync synchronisation"
